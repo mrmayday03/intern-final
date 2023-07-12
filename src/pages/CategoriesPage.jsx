439fe6696b48c3dc9b9d1b1d20349bc0704624b5
mrmayday03 <gmohan2803@gmail.com>
2023-07-12 22:12:46 +0530

    Added pages

---
+++ src/pages/CategoriesPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CategoryCards from '../tcomp/categories/CategoryCards' 
+const CategoriesPage = () => {
+  return (
+    <>
+        <CategoryCards />
+    </>
+  )
+}
+
+export default CategoriesPage
