c98b039dd3646f69b4aa5cf75dbb96dab2d7edec
mrmayday03 <gmohan2803@gmail.com>
2023-07-12 22:19:23 +0530

    Minor Change

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import StoreSupervisor from "./pages/StoreSupervisor";
 import CircularImage from "./components/CircularImage";
 import MensWear from "./pages/MensWear";
-import MainDash from "./pages/MainDash";
+import CategoriesPage from "./pages/CategoriesPage";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<CircularImage />} />
-        <Route path="/main" element={<MainDash />} />
+        <Route path="/main" element={<CategoriesPage />} />
         <Route path="/clothing" element={<StoreSupervisor />} />
         <Route path="/clothing/menswear" element={<MensWear />}></Route>
       </Routes>
--- src/tcomp/categories/CategoryCards.jsx
+++ src/tcomp/categories/CategoryCards.jsx
@@ -1,23 +1,27 @@
-
-import React from 'react';
-import './CategoryCards.css';
-import './CompScan.css'
-import cloth from '../../assets/icons/Cloths and Accessories.svg';
-import grocery from '../../assets/icons/Grocery.svg';
-import health from '../../assets/icons/Health and beauty.svg';
-import Home from '../../assets/icons/Home goods.svg';
-import toy from '../../assets/icons/Toys.svg';
-import elct from '../../assets/icons/electronic.svg';
-import DashBoardNav from '../dashbaordNav/DashBoardNav';
-import CompScan from './CompScan';
-import Scan from './Scan';
-import Sidebar from '../../components/Sidebar';
+import React from "react";
+import "./CategoryCards.css";
+import "./CompScan.css";
+import cloth from "../../assets/icons/Cloths and Accessories.svg";
+import grocery from "../../assets/icons/Grocery.svg";
+import health from "../../assets/icons/Health and beauty.svg";
+import Home from "../../assets/icons/Home goods.svg";
+import toy from "../../assets/icons/Toys.svg";
+import elct from "../../assets/icons/electronic.svg";
+import CompScan from "./CompScan";
+import Scan from "./Scan";
+import Sidebar from "../../components/Sidebar";
+import Header from "../../components/header";
 
 const CategoryCards = () => {
   return (
     <>
-    <DashBoardNav/>
-    <Sidebar/>
+      <div className="flex-1 flex-col h-screen">
+        <Header />
+        <div className="flex h-screen">
+          <Sidebar />
+        </div>
+      </div>
+
       <div className="category-cards">
         <div className="card-type ">
           <p className="card-title">Categories</p>
@@ -30,51 +34,50 @@ const CategoryCards = () => {
               </div>
               <span className="percent r">70%</span>
             </div>
-            <div className="category-item g"> 
+            <div className="category-item g">
               <div className="content g">
-                  <img src={health} className="content-img" alt="cloth" />
-                  <p className="type">Health & Beauty</p>
-                </div>
-                <span className="percent g">76%</span>
+                <img src={health} className="content-img" alt="cloth" />
+                <p className="type">Health & Beauty</p>
+              </div>
+              <span className="percent g">76%</span>
             </div>
-            <div className="category-item g"> 
+            <div className="category-item g">
               <div className="content g">
-                  <img src={toy} className="content-img" alt="cloth" />
-                  <p className="type">Toys</p>
-                </div>
-                <span className="percent g">76%</span>
+                <img src={toy} className="content-img" alt="cloth" />
+                <p className="type">Toys</p>
+              </div>
+              <span className="percent g">76%</span>
             </div>
           </div>
 
           <div className="column">
             <div className="category-item r">
-                <div className="content r">
-                  <img src={grocery} className="content-img" alt="cloth" />
-                  <p className="type">Grocery</p>
-                </div>
-                <span className="percent r">70%</span>
-            </div>       
-            <div className="category-item g"> 
+              <div className="content r">
+                <img src={grocery} className="content-img" alt="cloth" />
+                <p className="type">Grocery</p>
+              </div>
+              <span className="percent r">70%</span>
+            </div>
+            <div className="category-item g">
               <div className="content g">
-                  <img src={Home} className="content-img" alt="cloth" />
-                  <p className="type">Electronics</p>
-                </div>
-                <span className="percent g">70%</span> 
-            </div>          
-            <div className="category-item g"> 
+                <img src={Home} className="content-img" alt="cloth" />
+                <p className="type">Electronics</p>
+              </div>
+              <span className="percent g">70%</span>
+            </div>
+            <div className="category-item g">
               <div className="content g">
-                  <img src={elct} className="content-img" alt="cloth" />
-                  <p className="type">Electronics</p>
-                </div>
-                <span className="percent g">70%</span> 
-            </div>          
+                <img src={elct} className="content-img" alt="cloth" />
+                <p className="type">Electronics</p>
+              </div>
+              <span className="percent g">70%</span>
+            </div>
           </div>
 
-           <CompScan />
-           <Scan />
+          <CompScan />
+          <Scan />
         </div>
       </div>
-    
     </>
   );
 };
