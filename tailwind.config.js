7fe7be2c29beafb1c2287c8db1f6aaf54f11813b
Thanush19 <that.thanush.the.bloggy@gmail.com>
2023-07-18 16:26:37 +0530

    sdfs

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,8 @@ module.exports = {
     screens: {
       sm: "640px",
       md: "768px",
-      lg: "1024px",
-      xl: "1280px",
+      lg: "1100px",
+      xl: "1360px",
     },
     extend: {},
   },
