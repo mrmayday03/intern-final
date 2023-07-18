0928dd74e25c7a04447cc1a28ba9d300c2880f01
Thanush19 <that.thanush.the.bloggy@gmail.com>
2023-07-18 12:00:07 +0530

    tailwing,js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+// tailwind.config.js
+
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px', // Modify the value here
+      xl: '1280px',
+    },
     extend: {},
   },
   plugins: [
     require('tailwind-scrollbar-hide')
   ],
-}
-
+};
